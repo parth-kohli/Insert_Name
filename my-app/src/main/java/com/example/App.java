f59c6b0d94c81d73ba38c29adecb986bd4573ce2
Parth  Kohli <parthdabesthuman@gmail.com>
2025-08-13 22:27:15 +0530

    update: Change call order in App and support input provision

--- my-app/src/main/java/com/example/App.java
+++ my-app/src/main/java/com/example/App.java
@@ -2,8 +2,11 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven! lalala");
-        Preprocessor.main("Hi");
+        String text="India and Pakistan both possess nuclear weapons and fought their deadliest battle in decades in May, sparked by an attack on tourists the previous month in Indian Kashmir, which killed 26 civilians.\n" +
+                "Randhir Jaiswal, spokesperson for India's foreign ministry, said: \"Nuclear sabre-rattling is Pakistan’s stock-in-trade,\" adding: \"The international community can draw its own conclusions on the irresponsibility inherent in such remarks.\"\n" +
+                "He said it was also regrettable that the reported remarks should have been made while in a friendly third country.\n" +
+                "In a version of the speech shared by Pakistani security officials, Munir said: \"The (Indian) aggression has brought the region to the brink of a dangerously escalating war, where a bilateral conflict due to any miscalculation will be a grave mistake.\"";
+        Preprocessor.StopWordRemover(text);
     }
 }
 
