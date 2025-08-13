84600486eedbdb196952e039a89b7eec9972efc9
harinixw <itz.harini06@gmail.com>
2025-08-13 15:07:17 +0530

    feat: Add Preprocessor.java

--- my-app/src/main/java/com/example/App.java
+++ my-app/src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven! lala");
+        System.out.println("Hello, Maven! lalala");
         Preprocessor.main("Hi");
     }
 }
