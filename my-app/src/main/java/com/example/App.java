7a42f4c09e074ff5489b1ad7564fa2ea4d2ab021
parth-kohli <pkohli3307@gmail.com>
2025-08-13 15:03:33 +0530

    Update App.java

--- my-app/src/main/java/com/example/App.java
+++ my-app/src/main/java/com/example/App.java
@@ -2,7 +2,8 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven! lala");
+        System.out.println("Hello, Maven! ");
         Preprocessor.main("Hi");
     }
 }
+
