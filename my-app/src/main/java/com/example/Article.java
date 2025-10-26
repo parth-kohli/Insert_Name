438f1859218728fb5be325373c618773a023d2fe
Kevin Komban <kevinkomban@Kevins-MacBook-Pro.local>
2025-10-26 17:27:30 +0530

    Sorting and DB integration

---
+++ my-app/src/main/java/com/example/Article.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import java.util.Objects;
+
+public class Article {
+    final String title;
+    final String url;
+    final String sourceName;
+    final String publishedAt;
+    final String description;
+    final String imageUrl;
+    final String content;
+    final Float leftBias;
+    final Float centerBias;
+    final Float rightBias;
+
+    public Article(String title, String url, String sourceName, String publishedAt,
+                   String description, String imageUrl, String content, String category,
+                   Float leftBias, Float centerBias, Float rightBias) {
+        this.title = title;
+        this.url = url;
+        this.sourceName = sourceName;
+        this.publishedAt = publishedAt;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.content = content;
+        this.leftBias = leftBias;
+        this.centerBias = centerBias;
+        this.rightBias = rightBias;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Article article = (Article) obj;
+        return Objects.equals(url, article.url);
+    }
+}
